d235520ce04a519c7d78677f88708c652c45bb29
Jannis Eichborn <janniseichborn@gmail.com>
2026-01-05 22:07:53 +0100

    Day 1 part 1

---
+++ src/day_1.rs
@@ -0,0 +1,57 @@
+use std::fs;
+
+pub fn part_1() -> u32 {
+    let contents =
+        fs::read_to_string("inputs/day_1.txt").expect("Should have been able to read the file");
+    let (l, r) = parse_input(&contents);
+    compute_distance(l, r)
+}
+
+fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let mut l: Vec<u32> = Vec::new();
+    let mut r: Vec<u32> = Vec::new();
+
+    for line in input.lines() {
+        let (l_str, r_str) = line.trim().split_once(' ').expect("Invalid input");
+        l.push(l_str.trim().parse().expect("Invalid number"));
+        r.push(r_str.trim().parse().expect("Invalid number"));
+    }
+
+    l.sort();
+    r.sort();
+
+    (l, r)
+}
+
+fn compute_distance(l: Vec<u32>, r: Vec<u32>) -> u32 {
+    let mut sum: u32 = 0;
+    for i in 0..(l.len()) {
+        sum += l[i].abs_diff(r[i]);
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3";
+
+    #[test]
+    fn parse_input_test() {
+        let (l, r) = parse_input(TEST_INPUT);
+        assert_eq!(l.len(), r.len());
+    }
+
+    #[test]
+    fn distance_test() {
+        let (l, r) = parse_input(TEST_INPUT);
+        let sum = compute_distance(l, r);
+        assert_eq!(11, sum);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+mod day_1;
 use ferris_says::say;
 use std::io::{BufWriter, stdout};
 
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello wurstikus!");
+    let distance = day_1::part_1();
+    let message = format!("The distance is {}", distance);
 
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, 10, &mut writer).unwrap();
